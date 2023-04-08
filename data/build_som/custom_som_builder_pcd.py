070e813e04662e04cf8d0c51b6f7275e0edab915
bramdekker <b.dekker16@hotmail.com>
2023-04-08 16:22:16 +0200

    no transpose in som builder

--- data/build_som/custom_som_builder_pcd.py
+++ data/build_som/custom_som_builder_pcd.py
@@ -19,11 +19,11 @@ def som_saver_catenary_arches(root, rows, cols, gpu_ids, output_root):
         # Read las point cloud into array -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
         # f_las = laspy.read(os.path.join(root, f))
         f_pcd = o3d.io.read_point_cloud(os.path.join(root, f))
-        pc_np = np.asarray(f_pcd.points).T # 3xN tensor -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
+        pc_np = np.asarray(f_pcd.points) # 3xN tensor -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
         # pc_np = np.vstack((f_las.x, f_las.y, f_las.z)) # 3xN tensor -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
         # pc_np = np.transpose(pc_np) # Nx3 array -> [[x0, y0, z0], ..., [xn, yn, zn]]
 
-        sn_np = np.asarray(f_pcd.normals).T
+        sn_np = np.asarray(f_pcd.normals)
 
         # Downsample point cloud to take 524288 random points.
         pc_sampled = pc_np[:, np.random.choice(pc_np.shape[0], 524288, replace=False)] # 3 x sample_size
