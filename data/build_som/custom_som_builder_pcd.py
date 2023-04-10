7d956d7eed731874b856141b6e5aea4a9b90a8e7
bramdekker <b.dekker16@hotmail.com>
2023-04-10 10:44:34 +0200

    Transpose for reading in pcd

--- data/build_som/custom_som_builder_pcd.py
+++ data/build_som/custom_som_builder_pcd.py
@@ -19,13 +19,13 @@ def som_saver_catenary_arches(root, rows, cols, gpu_ids, output_root):
         # Read las point cloud into array -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
         # f_las = laspy.read(os.path.join(root, f))
         f_pcd = o3d.io.read_point_cloud(os.path.join(root, f))
-        pc_np = np.asarray(f_pcd.points) # 3xN tensor -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
-        print(f"Pc_np is like this: {pc_np[:10]}")
+        pc_np = np.asarray(f_pcd.points).T # 3xN tensor -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
+        print(f"Pc_np is like this: {pc_np[:, :3]}")
         # pc_np = np.vstack((f_las.x, f_las.y, f_las.z)) # 3xN tensor -> [[x0, ..., xn], [y0, ..., yn, [z0, ..., zn]]]
         # pc_np = np.transpose(pc_np) # Nx3 array -> [[x0, y0, z0], ..., [xn, yn, zn]]
 
-        sn_np = np.asarray(f_pcd.normals)
-        print(f"Sn_np is like this: {sn_np[:10]}")
+        sn_np = np.asarray(f_pcd.normals).T
+        print(f"Sn_np is like this: {sn_np[:, :3]}")
 
         # Downsample point cloud to take 524288 random points.
         print(f"PC_np shape[0] is thus {pc_np.shape[0]}")
