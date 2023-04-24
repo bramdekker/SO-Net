3a4bc48452ba6f7623df4afba9d188c6fc91e167
bramdekker <b.dekker16@hotmail.com>
2023-04-24 10:20:16 +0200

    Added slash in file path

--- data/build_som/custom_som_builder_pcd.py
+++ data/build_som/custom_som_builder_pcd.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    som_saver_catenary_arches(args.dir, args.rows, args.cols, 0, '%s%dx%d'%(args.dir, args.rows, args.cols))
+    som_saver_catenary_arches(args.dir, args.rows, args.cols, 0, '%s/%dx%d'%(args.dir, args.rows, args.cols))
 
 
     # if file[-3:] == 'txt':
